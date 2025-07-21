40aba378ec77adc0a2ca2960d6cdf54fa33b4da8
yusufguntav <ysf.guntav@gmail.com>
2025-07-21 16:41:11 +0300

    baseurl

--- crmclient.go
+++ crmclient.go
@@ -5,16 +5,18 @@ import (
 	"net/http"
 )
 
+var defaultBaseURL = "https://apicrm.vatansoft.net"
+
 type Client struct {
 	BaseURL    string
 	ProjectKey string
 	httpClient *http.Client
 }
 
-func New(baseURL, projectKey string) *Client {
+func New(projectKey string) *Client {
 	return &Client{
-		BaseURL:    baseURL,
 		ProjectKey: projectKey,
 		httpClient: &http.Client{},
+		BaseURL:    defaultBaseURL,
 	}
 }
